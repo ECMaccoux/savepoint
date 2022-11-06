5c738c756687265c3a98fa556e557ac82feddfe4
Eric Maccoux <43770992+ECMaccoux@users.noreply.github.com>
2022-11-06 14:33:53 -0600

    Initial experimental changes

--- App.js
+++ App.js
@@ -1,14 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Stack = createStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +12,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+function UseEffectExample() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <View style={styles.container}>
+      <Text>You clicked {count} times</Text>
+      <Button title="Press" onPress={() => setCount(count + 1)}></Button>
+    </View>
+  );
+}
+
+export default UseEffectExample;
